669e93a36f83ceb65aceb4ee9fdaea7a856f305a
Juan Soto <juan@juansoto.me>
2017-04-12 15:35:06 -0400

    Add Hex attributes

--- mix.exs
+++ mix.exs
@@ -8,7 +8,9 @@ defmodule LHeap.Mixfile do
       elixir: "~> 1.3",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
-      deps: deps()
+      deps: deps(),
+      description: description(),
+      package: package()
     ]
   end
 
@@ -20,6 +22,20 @@ defmodule LHeap.Mixfile do
     [
       {:excheck, "~> 0.5", only: :test},
       {:triq, github: "triqng/triq", only: :test}
+    ]
+  end
+
+  defp description do
+    """
+    Leftist heap in Elixir
+    """
+  end
+
+  defp package do
+    [
+      maintainers: ["sotojuan"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/sotojuan/lheap"}
     ]
   end
 end
